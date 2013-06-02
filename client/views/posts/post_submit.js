55606638649a4f95bb15e71464dd542bff167e3c
chendriksen <christiaan.hendriksen@gmail.com>
2013-06-02 14:21:00 +0100

    Monitor which errors have been seen, and clear on routing.

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -1,13 +1,11 @@
 Template.postSubmit.events({
   'submit form': function(event) {
     event.preventDefault();
-
     var post = {
       url: $(event.target).find('[name=url]').val(),
       title: $(event.target).find('[name=title]').val(),
       message: $(event.target).find('[name=message]').val()
     }
-
     Meteor.call('post', post, function(error, id) {
       if (error) {
         // display the error to the user
